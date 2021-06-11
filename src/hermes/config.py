04614b27739f0fff8368531a07c958f95fa854e8
Igor Montagner <igordsm@gmail.com>
2021-06-11 15:31:45 -0300

    Carrega credenciais de email do .config.

--- src/hermes/config.py
+++ src/hermes/config.py
@@ -1,5 +1,7 @@
 import os
 import os.path as osp
+from hermes.mail_util import load_mail_credentials
+
 
 def find_in_dir(dir, filename):
     return osp.exists(osp.join(dir, filename))
@@ -10,11 +12,10 @@ current_dir = os.getcwd()
 config_dir = osp.expanduser('~/.config/hermes/')
 
 if find_in_dir(current_dir, mail_credentials_name):
-    mail_credentials = osp.join(current_dir, mail_credentials_name)
+    mail_credentials_file = osp.join(current_dir, mail_credentials_name)
 elif find_in_dir(config_dir, mail_credentials_name):
-    mail_credentials = osp.join(current_dir, mail_credentials_name)
+    mail_credentials_file = osp.join(config_dir, mail_credentials_name)
 else:
-    mail_credentials = ''
+    mail_credentials_file = ''
 
-
-    
+mail_credentials = load_mail_credentials(mail_credentials_file)
